84763ea6400090601c62d8e064e6c265c2fdda83
failedman <2711339704@qq.com>
2026-07-15 10:11:23 +0800

    20260715

---
+++ build_guard_server/src/main/java/com/zxylearn/build_guard_server/BuildGuardServerApplication.java
@@ -0,0 +1,13 @@
+package com.zxylearn.build_guard_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildGuardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildGuardServerApplication.class, args);
+	}
+
+}
---
+++ build_guard_server/src/test/java/com/zxylearn/build_guard_server/BuildGuardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxylearn.build_guard_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildGuardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
